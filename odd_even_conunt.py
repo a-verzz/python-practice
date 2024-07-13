4bf68e7c1a620908fc54f0cf5d297d95bea5f047
a-verzz <100010257+a-verzz@users.noreply.github.com>
2024-07-13 14:55:54 +0530

    Create odd_even_conunt.py

---
+++ odd_even_conunt.py
@@ -0,0 +1,18 @@
+# A program that reads a sequence of numbers
+# and counts how many numbers are even and how many are odd.
+# The program terminates when zero is entered.
+ 
+odd_numbers = 0
+even_numbers = 0
+ 
+number = int(input("Enter a number or type 0 to stop: "))
+ 
+while number:
+    if number % 2 == 1:
+        odd_numbers += 1
+    else:
+        even_numbers += 1
+    number = int(input("Enter a number or type 0 to stop: "))
+ 
+print("Odd numbers count:", odd_numbers)
+print("Even numbers count:", even_numbers)
